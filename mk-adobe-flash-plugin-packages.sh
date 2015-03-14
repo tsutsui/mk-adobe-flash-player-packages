712c16d0b4622aabbfc67af1f98ec427d1b67cdc
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2015-03-15 05:11:25 +0900

    Update for 11.2.202.451.

--- mk-adobe-flash-plugin-packages.sh
+++ mk-adobe-flash-plugin-packages.sh
@@ -52,7 +52,7 @@
 #  - Needs more sane shell script implementation (functions etc).
 #
 
-FLASH_VERSION=11.2.202.442
+FLASH_VERSION=11.2.202.451
 
 # check platform and setup platform specific values
 MACHINE_ARCH=`uname -p`
