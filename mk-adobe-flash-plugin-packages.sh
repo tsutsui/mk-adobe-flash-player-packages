0e78ac4fde053482f9c8b5c921df1a9924831224
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2016-06-17 01:48:06 +0900

    Update for 11.2.202.626.

--- mk-adobe-flash-plugin-packages.sh
+++ mk-adobe-flash-plugin-packages.sh
@@ -52,7 +52,7 @@
 #  - Needs more sane shell script implementation (functions etc).
 #
 
-FLASH_VERSION=11.2.202.621
+FLASH_VERSION=11.2.202.626
 PKGREVISION=
 
 # check platform and setup platform specific values
